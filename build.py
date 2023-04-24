5f743bac698b9cb58e509b808c7e9e0fc11cafd1
Rémy Marquis <remy.marquis@heig-vd.ch>
2023-04-24 14:33:11 +0200

    Added all modules to dist packaging
    * also adjusted build.py for all modules

--- build.py
+++ build.py
@@ -6,7 +6,23 @@ def build(setup_kwargs):
     ext_modules = [
         Pybind11Extension(
             "example",
-            sorted(glob("*.cpp"))
+            sorted(glob("cpp/src/*.cpp"))
+        ),
+        Pybind11Extension(
+            "dimlp",
+            sorted(glob("cpp/src/*.cpp") + glob("cpp/src/*.h"))
+        ),
+        Pybind11Extension(
+            "fidex",
+            sorted(glob("cpp/src/*.cpp") + glob("cpp/src/*.h"))
+        ),
+        Pybind11Extension(
+            "fidexGlo",
+            sorted(glob("cpp/src/*.cpp") + glob("cpp/src/*.h"))
+        ),
+        Pybind11Extension(
+            "hyperLocus",
+            sorted(glob("cpp/src/*.cpp") + glob("cpp/src/*.h"))
         ),
     ]
     setup_kwargs.update({
