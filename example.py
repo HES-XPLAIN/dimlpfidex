3c22496de4f0425c440470c21f3be04c037b3134
Rémy Marquis <remy.marquis@heig-vd.ch>
2023-04-26 13:02:53 +0200

    Adjusted dist packaging
    * disabled compilation of example module
    * adjusted provided example file
    * adjusted path for compiled lib
    * adjusted include path for dist

--- example.py
+++ example.py
@@ -5,12 +5,14 @@ if sys.platform == 'win32':
     dir_path = os.path.abspath('example')
     os.add_dll_directory(dir_path)
 
-from example import example
+# from example import example
+from dimlp import dimlp
 
 def main():
-    print(example.cpp_hello())
-    print(example.cpp_add(7, 5))
-    print(example.cpp_fib(7))
+    # print(example.cpp_hello())
+    # print(example.cpp_add(7, 5))
+    # print(example.cpp_fib(7))
+    print(dir(dimlp))
 
 
 if __name__ == '__main__':
