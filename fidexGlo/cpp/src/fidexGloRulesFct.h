7b655494684840e0569e3afdbbdbbabcc77fc99d
Jean-Marc Boutay <jeanmarc.boutay@gmail.com>
2023-05-05 16:49:28 +0200

    fidexGloRules is now a function, can save results on file et has to choose folder to save

---
+++ fidexGlo/cpp/src/fidexGloRulesFct.h
@@ -0,0 +1,19 @@
+#ifndef FIDEXGLORULESFCT_H
+#define FIDEXGLORULESFCT_H
+
+#include "checkFun.h"
+#include "dataSet.h"
+#include "fidexAlgo.h"
+#include <algorithm>
+#include <chrono>
+#include <limits.h>
+#include <numeric>
+#include <random>
+#include <sstream>
+#include <string>
+#include <time.h>
+
+static void showParams();
+int fidexGloRules(string command);
+
+#endif
