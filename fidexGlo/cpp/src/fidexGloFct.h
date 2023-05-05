675227b02c42ebe6010eb15fc25ee3aed3ba0a2d
Jean-Marc Boutay <jeanmarc.boutay@gmail.com>
2023-05-05 15:03:55 +0200

    fidexGlo is now a function

---
+++ fidexGlo/cpp/src/fidexGloFct.h
@@ -0,0 +1,16 @@
+#ifndef FIDEXGLOFCT_H
+#define FIDEXGLOFCT_H
+
+#include "getRulesFun.h"
+#include <algorithm>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <time.h>
+#include <tuple>
+#include <vector>
+
+static void showParams();
+int fidexGlo(string command);
+
+#endif
