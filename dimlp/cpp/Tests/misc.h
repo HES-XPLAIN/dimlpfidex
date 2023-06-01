116c163af9131f16135b1522192a101222543da3
Jean-Marc Boutay <jeanmarc.boutay@gmail.com>
2023-06-01 14:23:38 +0200

    Removed Tests folder and corrected one bug

--- dimlp/cpp/Tests/misc.h
+++
@@ -1,9 +0,0 @@
-#ifndef MISC_H
-#define MISC_H
-#include "stringI.h"
-
-using namespace std;
-extern int Compare(const void *x, const void *y);
-extern int AreSameClass(StringInt *listPat, int *classPatNet);
-
-#endif
