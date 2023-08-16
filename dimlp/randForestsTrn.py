27680b9bea2bd6c60c98d92229fbd8c4bcc7e825
Jean-Marc-B <jeanmarc.boutay@gmail.com>
2023-08-16 15:26:46 +0200

    RandomForests : We get rules

--- dimlp/randForestsTrn.py
+++ dimlp/randForestsTrn.py
@@ -1,8 +1,51 @@
 import time
 import sys
 from sklearn import metrics
-from .trnFun import get_data, output_pred, output_stats
+from .trnFun import get_data, output_pred, output_stats, check_parameters_common, check_int, check_strictly_positive
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.tree import _tree
+import numpy as np
+
+
+def recurse(tree, node, parent_path, feature_names, output_rules_file, k_dict): # parent_path : path taken until current node
+    if tree.feature[node] != _tree.TREE_UNDEFINED: # Check if this is a real node
+        feature_name = feature_names[node] # Get node's feature name
+        threshold = tree.threshold[node] # Get node threshold
+        if node == 0:
+            left_path = f"{feature_name} <= {threshold}"
+        else:
+            left_path = f"{parent_path} & {feature_name} <= {threshold}"
+        recurse(tree, tree.children_left[node], left_path, feature_names, output_rules_file, k_dict) # Check left child node
+
+        if node == 0:
+            right_path = f"{feature_name} > {threshold}"
+        else:
+            right_path = f"{parent_path} & {feature_name} > {threshold}"
+        recurse(tree, tree.children_right[node], right_path, feature_names, output_rules_file, k_dict) # Check right child node
+    else: # If this is a leaf
+        k = k_dict["value"]
+        k += 1
+        k_dict["value"] = k
+        cover_value = tree.value[node] # Get cover values for this rule
+        output_rules_file.write(f"Rule {k}: {parent_path} -> class {np.argmax(cover_value)} Covering: {[int(num) for num in cover_value[0]]}\n") # Write rule
+
+def trees_to_rules(trees, rules_file):
+    try:
+        with open(rules_file, "w") as output_rules_file:
+            for t in range(len(trees)):
+                output_rules_file.write(f"-------------------\nTree {t + 1}\n-------------------\n")
+                tree_ = trees[t].tree_
+                feature_names = [
+                    "X"+str(i) if i != _tree.TREE_UNDEFINED else "undefined!"
+                    for i in tree_.feature
+                ]
+                k_dict = {"value": 0}
+                recurse(tree_, 0, "", feature_names, output_rules_file, k_dict)
+            output_rules_file.close()
+    except FileNotFoundError:
+        raise ValueError(f"Error: File for rules extraction ({rules_file}) not found.")
+    except IOError:
+        raise ValueError(f"Error: Couldn't open rules extraction file {rules_file}.")
 
 def randForestsTrn(*args, **kwargs):
     try:
@@ -24,12 +67,14 @@ def randForestsTrn(*args, **kwargs):
             print("test_pred : output test prediction file name without extension(predTest by default)")
             print("stats : output file name without extension with train and test accuracy")
             print("output_file : file where you redirect console result")
+            print("rules_file : random forests rules file without extension (RF_rules.rls by default)")
             print("----------------------------")
             print("SVM parameters (optional)")
+            print("n_estimators : Number of generated trees in the forest(100 by default)")
             print("----------------------------")
             print("----------------------------")
             print("Here is an example, keep same parameter names :")
-            print('randForestsTrn(train_data="datanormTrain",train_class="dataclass2Train", test_data="datanormTest",test_class="dataclass2Test", stats = "rf/stats.txt", train_pred = "rf/predTrain", test_pred = "rf/predTest", save_folder = "dimlp/datafiles")')
+            print('randForestsTrn(train_data="datanormTrain",train_class="dataclass2Train", test_data="datanormTest",test_class="dataclass2Test", stats = "rf/stats.txt", train_pred = "rf/predTrain", test_pred = "rf/predTest", rules_file = "rf/RF_rules", save_folder = "dimlp/datafiles")')
             print("---------------------------------------------------------------------")
         else:
 
@@ -46,6 +91,9 @@ def randForestsTrn(*args, **kwargs):
             train_pred_file = kwargs.get('train_pred')
             test_pred_file = kwargs.get('test_pred')
             stats_file = kwargs.get('stats')
+            rules_file = kwargs.get('rules_file')
+
+            n_estimators_var = kwargs.get('n_estimators')
 
             # Redirect output in file
             if output_file != None:
@@ -61,14 +109,25 @@ def randForestsTrn(*args, **kwargs):
             # Check parameters
 
             valid_args = ['train_data', 'train_class', 'test_data', 'test_class', 'train_pred', 'test_pred',
-                        'stats', 'save_folder', 'output_file']
+                        'stats', 'save_folder', 'output_file', 'rules_file', 'n_estimators']
 
             # Check if wrong parameters are given
             for arg_key in kwargs.keys():
                 if arg_key not in valid_args:
                     raise ValueError(f"Invalid argument : {arg_key}")
 
+            save_folder, train_data_file, train_class_file, test_data_file, test_class_file, train_pred_file, test_pred_file, stats_file  = check_parameters_common(save_folder, train_data_file, train_class_file, test_data_file, test_class_file, train_pred_file, test_pred_file, stats_file)
 
+            if rules_file is None:
+                rules_file = "RF_rules"
+            elif not isinstance(rules_file, str):
+                raise ValueError('Error : parameter rules_file has to be a name contained in quotation marks "".')
+            rules_file += ".rls"
+
+            if n_estimators_var is None:
+                n_estimators_var = 100
+            elif (not check_strictly_positive(n_estimators_var) or not check_int(n_estimators_var)):
+                raise ValueError('Error, parameter n_estimators is not a strictly positive number')
 
             if (save_folder is not None):
                 train_data_file = save_folder + "/" + train_data_file
@@ -77,6 +136,7 @@ def randForestsTrn(*args, **kwargs):
                 test_class_file = save_folder + "/" + test_class_file
                 train_pred_file = save_folder + "/" + train_pred_file
                 test_pred_file = save_folder + "/" + test_pred_file
+                rules_file = save_folder + "/" + rules_file
                 if (stats_file is not None):
                     stats_file = save_folder + "/" + stats_file
 
@@ -89,8 +149,7 @@ def randForestsTrn(*args, **kwargs):
             test_class = [cl.index(max(cl)) for cl in test_class]
 
             # Train RF
-            nb_trees = 100
-            model = RandomForestClassifier(n_estimators=nb_trees, verbose=1, oob_score=False) # Create RF Classifier
+            model = RandomForestClassifier(n_estimators=n_estimators_var, verbose=1, oob_score=False) # Create RF Classifier
 
             model.fit(train_data, train_class)   # Train the model using the training sets
             train_pred = model.predict(train_data)    # Predict the response for train dataset
@@ -111,8 +170,7 @@ def randForestsTrn(*args, **kwargs):
 
             # Output accuracy statistics
             output_stats(stats_file, acc_train, acc_test)
-
-            print(model.estimators_)
+            trees_to_rules(model.estimators_, rules_file)
 
             end_time = time.time()
             full_time = end_time - start_time
