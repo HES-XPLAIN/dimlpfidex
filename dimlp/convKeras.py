f0d26f0e93d454338c6b8f46b2d4831fedc78855
Jean-Marc-B <jeanmarc.boutay@gmail.com>
2023-09-14 10:24:40 +0200

    Changed validation in convKeras

--- dimlp/convKeras.py
+++ dimlp/convKeras.py
@@ -27,7 +27,7 @@ from keras.datasets import mnist
 from keras.datasets import cifar100
 
 
-from .trnFun import compute_first_hidden_layer, output_stats, check_parameters_dimlp_layer, check_parameters_common, get_data
+from .trnFun import compute_first_hidden_layer, output_stats, check_parameters_dimlp_layer, check_parameters_common, get_data, check_strictly_positive, check_int
 
 def output_pred(pred, pred_file):
     try:
@@ -52,12 +52,14 @@ def convKeras(*args, **kwargs):
             print("train_class : train class file")
             print("test_data : test data file")
             print("test_class : test class file")
-            print("valid_data : validation data file")
-            print("valid_class : validation class file")
+            print("valid_ratio : porcentage(]0,1[) of train data taken for validation (0.1 by default if no valid data)")
             print("----------------------------")
             print("Optional parameters :")
+            print("valid_data : validation data file")
+            print("valid_class : validation class file")
             print("save_folder : Folder based on main folder dimlpfidex(default folder) where generated files will be saved. If a file name is specified with another option, his path will be configured with respect to this root folder.")
-            print("train_pred : output train prediction file name without extension(predTrain by default)")
+            print("nb_epochs : number of epochs during training(80 by default)")
+            print("train_valid_pred : output train and validation (in this order) prediction file name without extension(predTrain by default)")
             print("test_pred : output test prediction file name without extension(predTest by default)")
             print("weights : output weights file name without extension (weights by default)")
             print("stats : output file name with train and test accuracy (stats.txt by default)")
@@ -76,16 +78,18 @@ def convKeras(*args, **kwargs):
             train_class_file = kwargs.get('train_class')
             test_data_file = kwargs.get('test_data')
             test_class_file = kwargs.get('test_class')
+            valid_ratio = kwargs.get('valid_ratio')
             valid_data_file = kwargs.get('valid_data')
             valid_class_file = kwargs.get('valid_class')
             output_file = kwargs.get('output_file')
-            train_pred_file = kwargs.get('train_pred')
+            train_valid_pred_file = kwargs.get('train_valid_pred')
             test_pred_file = kwargs.get('test_pred')
             weights_file = kwargs.get('weights')
             stats_file = kwargs.get('stats')
             K = kwargs.get('K')
             quant = kwargs.get('nb_stairs')
             hiknot = kwargs.get('hiknot')
+            nb_epochs = kwargs.get('nb_epochs')
 
             # Redirect output in file
             if output_file != None:
@@ -98,29 +102,41 @@ def convKeras(*args, **kwargs):
                 except (IOError):
                     raise ValueError(f"Error : Couldn't open file {output_file}.")
 
-            valid_args = ['dataset', 'train_data', 'train_class', 'test_data', 'test_class', 'valid_data', 'valid_class', 'save_folder', 'output_file', 'train_pred', 'test_pred', 'weights', 'stats', 'K', 'nb_stairs', 'hiknot']
+            valid_args = ['dataset', 'train_data', 'train_class', 'test_data', 'test_class', 'valid_ratio', 'valid_data', 'valid_class', 'save_folder', 'output_file', 'train_valid_pred', 'test_pred', 'weights',
+                          'stats', 'K', 'nb_stairs', 'hiknot', 'nb_epochs']
 
             # Check if wrong parameters are given
             for arg_key in kwargs.keys():
                 if arg_key not in valid_args:
                     raise ValueError(f"Invalid argument : {arg_key}")
 
-            save_folder, train_data_file, train_class_file, test_data_file, test_class_file, train_pred_file, test_pred_file, stats_file  = check_parameters_common(save_folder, train_data_file, train_class_file, test_data_file, test_class_file, train_pred_file, test_pred_file, stats_file)
+            save_folder, train_data_file, train_class_file, test_data_file, test_class_file, train_valid_pred_file, test_pred_file, stats_file  = check_parameters_common(save_folder, train_data_file, train_class_file, test_data_file, test_class_file, train_valid_pred_file, test_pred_file, stats_file)
+            if valid_ratio is None:
+                if (valid_data_file is None and valid_class_file is not None) or (valid_data_file is not None and valid_class_file is None):
+                    raise ValueError('Error : parameter valid_data_file or parameter valid_class_file missing.')
+            else:
+                if valid_data_file is not None or valid_class_file is not None:
+                    raise ValueError('Error : parameter valid_data_file or parameter valid_class_file is given but there is already a validation ratio.')
+            if valid_ratio is None and valid_data_file is None and valid_class_file is None:
+                valid_ratio = 0.1
+            elif valid_ratio is not None and (valid_ratio <= 0 or valid_ratio >= 1):
+                raise ValueError('Error : parameter valid_ratio has to be strictly bigger than 0 and strictly smaller than 1.')
 
-            if valid_data_file is None :
-                raise ValueError('Error : validation data file missing, add it with option valid_data="your_validation_data_file"')
-            elif not isinstance(valid_data_file, str):
+            if valid_data_file is not None and not isinstance(valid_data_file, str):
                 raise ValueError('Error : parameter valid_data has to be a name contained in quotation marks "".')
 
-            if valid_class_file is None :
-                raise ValueError('Error : validation class file missing, add it with option valid_class="your_validation_class_file"')
-            elif not isinstance(valid_class_file, str):
+            if valid_class_file is not None and not isinstance(valid_class_file, str):
                 raise ValueError('Error : parameter valid_class has to be a name contained in quotation marks "".')
 
             if dataset is None :
                 raise ValueError('Error : dataset name missing, add it with option dataset and choose "mnist" or "cifar".')
             elif dataset not in {"mnist", "cifar"}:
                 raise ValueError('Error : parameter dataset is not "mnist" or "cifar".')
+
+            if nb_epochs is None:
+                nb_epochs = 80
+            elif not check_int(nb_epochs) or not check_strictly_positive(nb_epochs):
+                raise ValueError('Error : parameter nb_epochs is not a strictly positive integer.')
 
             weights_file, K, quant, hiknot = check_parameters_dimlp_layer(weights_file, K, quant, hiknot)
 
@@ -130,18 +146,15 @@ def convKeras(*args, **kwargs):
                 train_class_file = save_folder + "/" + train_class_file
                 test_data_file = save_folder + "/" + test_data_file
                 test_class_file = save_folder + "/" + test_class_file
-                valid_data_file = save_folder + "/" + valid_data_file
-                valid_class_file = save_folder + "/" + valid_class_file
-                train_pred_file = save_folder + "/" + train_pred_file
+                if valid_ratio is None:
+                    valid_data_file = save_folder + "/" + valid_data_file
+                    valid_class_file = save_folder + "/" + valid_class_file
+                train_valid_pred_file = save_folder + "/" + train_valid_pred_file
                 test_pred_file = save_folder + "/" + test_pred_file
                 weights_file = save_folder + "/" + weights_file
                 if (stats_file is not None):
                     stats_file = save_folder + "/" + stats_file
                 model_checkpoint_weights = save_folder + "/" + model_checkpoint_weights
-
-            ###############################################################
-
-            nb_it   = 80
 
             ###############################################################
 
@@ -174,15 +187,25 @@ def convKeras(*args, **kwargs):
 
 
             # Get data
-            x_train = np.array(get_data(train_data_file))
-            y_train = get_data(train_class_file)
-            y_train = np.array([cl.index(max(cl)) for cl in y_train])
+            x_train_full = np.array(get_data(train_data_file))
+            y_train_full = get_data(train_class_file)
+            y_train_full = np.array([cl.index(max(cl)) for cl in y_train_full])
             x_test = np.array(get_data(test_data_file))
             y_test = get_data(test_class_file)
             y_test = np.array([cl.index(max(cl)) for cl in y_test])
-            x_val = np.array(get_data(valid_data_file))
-            y_val = get_data(valid_class_file)
-            y_val = np.array([cl.index(max(cl)) for cl in y_val])
+            if valid_ratio is None:
+                x_train = x_train_full
+                y_train = y_train_full
+                x_val = np.array(get_data(valid_data_file))
+                y_val = get_data(valid_class_file)
+                y_val = np.array([cl.index(max(cl)) for cl in y_val])
+            else:
+                cut_off = int(len(x_train_full)*(1-valid_ratio))
+                x_train = x_train_full[:cut_off]
+                x_val   = x_train_full[cut_off:]
+                y_train = y_train_full[:cut_off]
+                y_val   = y_train_full[cut_off:]
+
 
             x_train = (x_train.astype('float32') / 255) * 10 - 5
             x_test = (x_test.astype('float32') / 255) * 10 - 5
@@ -223,6 +246,7 @@ def convKeras(*args, **kwargs):
             y_train = keras.utils.to_categorical(y_train, nb_classes)
             y_test = keras.utils.to_categorical(y_test, nb_classes)
             y_val = keras.utils.to_categorical(y_val, nb_classes)
+
             ##############################################################################
 
             """
@@ -276,8 +300,8 @@ def convKeras(*args, **kwargs):
 
             checkpointer = ModelCheckpoint(filepath=model_checkpoint_weights, verbose=1, save_best_only=True)
 
-            model.fit(x_train_h1, y_train, batch_size=32, epochs=nb_it, validation_data=(x_val_h1, y_val), callbacks=[checkpointer], verbose=2)
-            #model.fit(x_train_h1_train, y_train, batch_size=32, epochs=nb_it, validation_data=(x_test_h1, y_test), callbacks=[checkpointer], verbose=2)
+            model.fit(x_train_h1, y_train, batch_size=32, epochs=nb_epochs, validation_data=(x_val_h1, y_val), callbacks=[checkpointer], verbose=2)
+            #model.fit(x_train_h1_train, y_train, batch_size=32, epochs=nb_epochs, validation_data=(x_test_h1, y_test), callbacks=[checkpointer], verbose=2)
 
             ##############################################################################
 
@@ -285,9 +309,11 @@ def convKeras(*args, **kwargs):
 
             train_pred = model_best.predict(x_train_h1)    # Predict the response for train dataset
             test_pred = model_best.predict(x_test_h1)    # Predict the response for test dataset
+            valid_pred = model_best.predict(x_val_h1)   # Predict the response for validation dataset
+            train_valid_pred = np.concatenate((train_pred,valid_pred))
 
             # Output predictions
-            output_pred(train_pred, train_pred_file)
+            output_pred(train_valid_pred, train_valid_pred_file)
             output_pred(test_pred, test_pred_file)
 
             ##############################################################################
