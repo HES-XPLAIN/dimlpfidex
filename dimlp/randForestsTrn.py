27753138208ce3f1fa443a59b4df88b3267d1236
Jean-Marc-B <jeanmarc.boutay@gmail.com>
2023-10-19 17:37:56 +0200

    Added verification that all classes and represented when trianing

--- dimlp/randForestsTrn.py
+++ dimlp/randForestsTrn.py
@@ -163,7 +163,10 @@ def randForestsTrn(*args, **kwargs):
             # Get data
             train_data = get_data(train_data_file)
             train_class = get_data(train_class_file)
+            nb_classes = len(train_class[0])
+            classes = set(range(nb_classes))
             train_class = [cl.index(max(cl)) for cl in train_class]
+            miss_train_classes = classes - set(train_class) # Check if a class is not represented
             test_data = get_data(test_data_file)
             test_class = get_data(test_class_file)
             test_class = [cl.index(max(cl)) for cl in test_class]
@@ -183,9 +186,17 @@ def randForestsTrn(*args, **kwargs):
             train_pred = model.predict(train_data)    # Predict the response for train dataset
             test_pred = model.predict(test_data)    # Predict the response for test dataset
 
+            # If a class is missing, we adapt predictions
+            if len(miss_train_classes)!=0:
+                miss_train_classes = sorted(list(miss_train_classes))
+                train_pred_proba = [pred.tolist() for pred in train_pred_proba]
+                test_pred_proba = [pred.tolist() for pred in test_pred_proba]
+                for train_pred_list in [train_pred_proba, test_pred_proba]:
+                    for pred in train_pred_list:
+                        for classe in miss_train_classes:
+                            pred.insert(classe, 0.0) # Prediction 0 for the missing class
 
             # Output predictions
-            nb_classes = len(model.classes_)
             output_pred_proba(train_pred_proba, train_pred_file)
             output_pred_proba(test_pred_proba, test_pred_file)
 
