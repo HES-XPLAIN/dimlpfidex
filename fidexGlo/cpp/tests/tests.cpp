950b4d18adec61bc9e68d41abc9130b339829de4
Damian Boquete <damian.boquete@hotmail.com>
2024-01-25 10:38:36 +0100

    Working on tests, not usable yet

--- fidexGlo/cpp/tests/tests.cpp
+++ fidexGlo/cpp/tests/tests.cpp
@@ -4,35 +4,36 @@ void test_help_output() {
   map<string, string> args = {};
   string argsStr = argsToString(args);
 
+  // TODO: catch exit(1) or find something else
   testAssert("Help output", fidexGloRules(argsStr) != 0);
 }
 
-void test_train_data_file() {
-  map<string, string> args = remove(DEFAULT_ARGS, {"-T"});
+// TODO continue here
+void test_missing_parameter(const std::string &parameterName) {
+  map<string, string> args = remove(DEFAULT_ARGS, {"--data_train_file"});
   string argsStr = argsToString(args);
 
   try {
     fidexGloRules(argsStr);
     testAssert("Train data file missing", false);
-  } catch (CommandArgumentException e) {
-    testAssert("Train data file missing", true);
-    cout << e.what() << endl;
+
+  } catch (CommandArgumentException &e) {
+    std::string expectedException = "Parameters error: argument (ID " + parameterName + ") requested was not found, try to rerun including it.";
+    std::string actualException(e.what());
+    int diff = expectedException.compare(actualException);
+
+    testAssert("Train data file missing", diff == 0);
   }
 }
 
-void test_parameters_from_json_file() {
-  // auto p = Parameters(DEFAULT_ARGS);
-  auto p = Parameters(JSON_CONFIG_FILE);
-
-  cout << p;
-}
-
-// TODO continue here
+// void test_parameters_from_json_file() {
+//   auto p = Parameters(DEFAULT_JSON_CONFIG_FILE);
+// }
 
 int main(int argc, char **argv) {
-  // test_help_output();
-  // test_train_data_file();
-  test_parameters_from_json_file();
+  test_help_output();
+  // test_missing_parameter();
+  // test_parameters_from_json_file();
 
   return EXIT_SUCCESS;
 }
--- fidexGlo/cpp/tests/tests.h
+++ fidexGlo/cpp/tests/tests.h
@@ -1,84 +1,53 @@
 #ifndef TESTS_H
 #define TESTS_H
 
-// -------------------------------------------------
-// Obligatory parameters :
-// -T <train data file>
-// -P <train prediction file>
-// -C <train true class file, not mendatory if classes are specified in train data file>
-// -W <weights file. In case of bagging, put prefix of files, ex: DimlpBT, files need to be in the form DimlpBTi.wts, i=1,2,3,... and you need to specify the number of networks with -N> [Not mendatory if a rules file is given with -f]
-// -f <rules file to be converted to hyperlocus> [Not mendatory if a weights file is given]
-// -O <Rules output file>
-// -M <Heuristic 1: optimal fidexGlo, 2: fast fidexGlo 3: very fast fidexGlo>
-// -a <number of attributes>
-// -b <number of classes><Options>
-//
-// Options are:
-// -S <Folder based on main folder dimlpfidex(default folder) where generated files will be saved. If a file name is specified with another option, his path will be configured with respect to this root folder>
-// -N <number of networks for bagging, 1 means no bagging, necessary to use bagging (1 by default)>
-// -A <file of attributes>
-// -r <file where you redirect console result>
-// -i <max iteration number (100 by default)>
-// -v <minimum covering number (2 by default)>
-// -d <dimension dropout parameter>
-// -h <hyperplan dropout parameter>
-// -m <maximum number of failed attempts to find Fidex rule when covering is 1 (30 by default)>
-// -Q <number of stairs in staircase activation function (50 by default)>
-// -t <decision threshold for predictions, need to specify the index of positive class if you want to use it (None by default)>
-// -x <index of positive class for the usage of decision threshold (None by default, 0 for first one)>
-// -p <number of threads used for computing the algorithm (default=1, this means by default its a sequential execution)>
-// -y <minimal rule fidelity accepted when generating a rule [0,1] (default=1)>
-// -z <seed (0=random, default)>
-// -------------------------------------------------
-
 #include "../../../fidexCommon/cpp/src/parameters.h"
 #include "../src/fidexGloRulesFct.h"
 #include <map>
-
-// define default values to avoid litterals redundancy
-#define PROGRAM_NAME "fidexGloRules"
-#define DATA_FOLDER "./dataset/data/"
-#define OUT_FOLDER "./dataset/out/"
-#define JSON_CONFIG_FILE "/home/eldado/Documents/hepia/dimlpfidex-json/dimlpfidex/fidexGlo/cpp/tests/config.json"
-
-// default paths
-#define TRAIN_FILE DATA_FOLDER "train.txt"
-#define TRAIN_PRED_FILE DATA_FOLDER "train.out"
-#define TRAIN_TRUE_CLASS_FILE DATA_FOLDER "train_true_classes.txt"
-#define WEIGHTS_FILE DATA_FOLDER "weights.txt"
-#define TXT_IN_RULES_FILE DATA_FOLDER "rules.txt"
-#define TXT_OUT_RULES_FILE OUT_FOLDER "new_rules.txt"
-#define JSON_IN_RULES_FILE DATA_FOLDER "rules.json"
-#define JSON_OUT_RULES_FILE OUT_FOLDER "new_rules.json"
-#define ATTRIBUTES_FILE DATA_FOLDER "attributes.txt"
-#define CONSOLE_FILE OUT_FOLDER "console.out"
-
-// numeric args
-#define HEURISTIC "1"
-#define NB_ATTRIBUTES "31"
-#define NB_CLASSES "7"
-#define NB_BAGGING_NETS "1"
-#define MAX_ITERATIONS "100"
-#define MIN_COVERING "2"
-#define DROPOUT_DIM "0.3"
-#define DROPOUT_HYP "0.3"
-#define MAX_FAILED_ATTEMPTS "10"
-#define NB_STAIRS_STAIRCASE_FN "50"
-#define DECISION_THRESHOLD "-1.0"
-#define IDX_POSITIVE_CLASS "-1"
-#define NB_THREADS "4"
-#define MIN_FIDELITY "1.0"
-#define SEED "0"
 
 // printing utils
 #define RED "\033[1;31m"
 #define GREEN "\033[1;32m"
 #define RESET "\033[0m"
 
-using namespace std;
+// default values to avoid generating litterals redundancy
+static const std::string PROGRAM_NAME = "fidexGloRules";
+static const std::string DATA_FOLDER = "./dataset/data/";
+static const std::string OUT_FOLDER = "./dataset/out/";
+// TODO: find a way to include relative path instead
+static const std::string DEFAULT_JSON_CONFIG_FILE = "/home/eldado/Documents/hepia/dimlpfidex-json/dimlpfidex/fidexGlo/cpp/tests/config.json";
 
-string argsToString(const map<string, string> &args) {
-  stringstream ss;
+// default paths
+static const std::string DEFAULT_TRAIN_FILE = DATA_FOLDER + "train.txt";
+static const std::string DEFAULT_TRAIN_PRED_FILE = DATA_FOLDER + "train.out";
+static const std::string DEFAULT_TRAIN_TRUE_CLASS_FILE = DATA_FOLDER + "train_true_classes.txt";
+static const std::string DEFAULT_WEIGHTS_FILE = DATA_FOLDER + "weights.txt";
+static const std::string DEFAULT_TXT_IN_RULES_FILE = DATA_FOLDER + "rules.txt";
+static const std::string DEFAULT_TXT_OUT_RULES_FILE = OUT_FOLDER + "new_rules.txt";
+static const std::string DEFAULT_JSON_IN_RULES_FILE = DATA_FOLDER + "rules.json";
+static const std::string DEFAULT_JSON_OUT_RULES_FILE = OUT_FOLDER + "new_rules.json";
+static const std::string DEFAULT_ATTRIBUTES_FILE = DATA_FOLDER + "attributes.txt";
+static const std::string DEFAULT_CONSOLE_FILE = OUT_FOLDER + "console.out";
+
+// default numeric args
+static const std::string DEFAULT_HEURISTIC = "1";
+static const std::string DEFAULT_NB_ATTRIBUTES = "31";
+static const std::string DEFAULT_NB_CLASSES = "7";
+static const std::string DEFAULT_NB_BAGGING_NETS = "1";
+static const std::string DEFAULT_MAX_ITERATIONS = "100";
+static const std::string DEFAULT_MIN_COVERING = "2";
+static const std::string DEFAULT_DROPOUT_DIM = "0.3";
+static const std::string DEFAULT_DROPOUT_HYP = "0.3";
+static const std::string DEFAULT_MAX_FAILED_ATTEMPTS = "10";
+static const std::string DEFAULT_NB_STAIRS_STAIRCASE_FN = "50";
+static const std::string DEFAULT_DECISION_THRESHOLD = "-1.0";
+static const std::string DEFAULT_IDX_POSITIVE_CLASS = "-1";
+static const std::string DEFAULT_NB_THREADS = "4";
+static const std::string DEFAULT_MIN_FIDELITY = "1.0";
+static const std::string DEFAULT_SEED = "0";
+
+std::string argsToString(const map<std::string, std::string> &args) {
+  std::stringstream ss;
   for (const auto &kv : args) {
     ss << kv.first << " " << kv.second << " ";
   }
@@ -86,17 +55,17 @@ string argsToString(const map<string, string> &args) {
   return ss.str();
 }
 
-map<string, string> remove(const map<string, string> &args, vector<string> toRemove) {
-  map<string, string> result = args;
+map<std::string, std::string> remove(const map<std::string, std::string> &args, const vector<std::string> &toRemove) {
+  map<std::string, std::string> result = args;
 
-  for (string item : toRemove) {
+  for (std::string item : toRemove) {
     result.erase(item);
   }
 
   return result;
 }
 
-void testAssert(string testName, bool condition) {
+void testAssert(const std::string &testName, bool condition) {
   if (condition) {
     cout << GREEN << "[" << testName << "] "
          << "passed" << RESET << endl;
@@ -106,30 +75,31 @@ void testAssert(string testName, bool condition) {
   }
 }
 
-const map<string, string> DEFAULT_ARGS{
-    {"-T", TRAIN_FILE},
-    {"-P", TRAIN_PRED_FILE},
-    {"-C", TRAIN_TRUE_CLASS_FILE},
-    {"-W", WEIGHTS_FILE},
-    // {"-f", TXT_IN_RULES_FILE}, // TODO
-    {"-O", TXT_OUT_RULES_FILE},
-    {"-M", HEURISTIC},
-    {"-a", NB_ATTRIBUTES},
-    {"-b", NB_CLASSES},
-    // {"-S", DATA_FOLDER}, // TODO
-    {"-N", NB_BAGGING_NETS},
-    {"-A", ATTRIBUTES_FILE},
-    {"-r", CONSOLE_FILE},
-    {"-i", MAX_ITERATIONS},
-    {"-v", MIN_COVERING},
-    {"-d", DROPOUT_DIM},
-    {"-h", DROPOUT_HYP},
-    {"-m", MAX_FAILED_ATTEMPTS},
-    {"-Q", NB_STAIRS_STAIRCASE_FN},
-    {"-t", DECISION_THRESHOLD},
-    {"-x", IDX_POSITIVE_CLASS},
-    {"-p", NB_THREADS},
-    {"-y", MIN_FIDELITY},
-    {"-z", SEED}};
+const map<std::string, std::string> DEFAULT_ARGS{
+    {"--train_data_file", DEFAULT_TRAIN_FILE},
+    {"--train_pred_file", DEFAULT_TRAIN_PRED_FILE},
+    {"--train_class_file", DEFAULT_TRAIN_TRUE_CLASS_FILE},
+    // {"--rules_file", DEFAULT_TXT_IN_RULES_FILE}, // TODO
+    {"--rules_outfile", DEFAULT_TXT_OUT_RULES_FILE},
+    {"--console_file", DEFAULT_CONSOLE_FILE},
+    // {"--root_folder", DEFAULT_ROOT_FOLDER}, // TODO
+    {"--attributes_file", DEFAULT_ATTRIBUTES_FILE},
+    {"--weights_file", DEFAULT_WEIGHTS_FILE},
+    {"--nb_attributes", DEFAULT_NB_ATTRIBUTES},
+    {"--nb_classes", DEFAULT_NB_CLASSES},
+    {"--nb_dimlp_nets", DEFAULT_NB_BAGGING_NETS},
+    {"--nb_quant_levels", DEFAULT_NB_STAIRS_STAIRCASE_FN},
+    {"--heuristic", DEFAULT_HEURISTIC},
+    {"--max_iterations", DEFAULT_MAX_ITERATIONS},
+    {"--min_covering", DEFAULT_MIN_COVERING},
+    {"--max_failed_attempts", DEFAULT_MAX_FAILED_ATTEMPTS},
+    {"--nb_threads", DEFAULT_NB_THREADS},
+    {"--positive_class_index", DEFAULT_IDX_POSITIVE_CLASS},
+    {"--seed", DEFAULT_SEED},
+    {"--decision_threshold", DEFAULT_DECISION_THRESHOLD},
+    // {"--hi_knot", HI_KNOT}, // TODO
+    {"--dropout_hyp", DEFAULT_DROPOUT_DIM},
+    {"--dropout_dim", DEFAULT_DROPOUT_HYP},
+    {"--min_fidelity", DEFAULT_MIN_FIDELITY}};
 
 #endif // TESTS_H
