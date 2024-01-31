d8dcfed7e03edca99924e9365ebe56b043438d40
Damian Boquete <damian.boquete@hotmail.com>
2024-01-31 16:16:08 +0100

    Added some unit tests

--- fidexTests/cpp/src/fidexCommonTests.cpp
+++ fidexTests/cpp/src/fidexCommonTests.cpp
@@ -33,8 +33,15 @@ void test_parameters_setter() {
 
 void test_parameters_getter() {
   Parameters p;
+  p.setString(ROOT_FOLDER, "folder/");
+  p.setInt(HEURISTIC, 1);
   p.setFloat(DROPOUT_DIM, 0.5f);
-  testAssert("Parameter get value", p.getFloat(DROPOUT_DIM) == 0.5f);
+  p.setDouble(DROPOUT_HYP, 0.99999);
+
+  testAssert("Parameter get correct string value", p.getString(ROOT_FOLDER).compare("folder/") == 0);
+  testAssert("Parameter get correct int value", p.getInt(HEURISTIC) == 1);
+  testAssert("Parameter get correct float value", p.getFloat(DROPOUT_DIM) == 0.5f);
+  testAssert("Parameter get correct double value", p.getDouble(DROPOUT_HYP) == 0.99999);
 
   try {
     p.getString(TRAIN_DATA_FILE);
@@ -49,22 +56,41 @@ void test_parameters_getter() {
 
 void test_parameters_default_setter() {
   Parameters p;
-
   std::string value = "somewhere/over/the/rainbow/";
+  std::string new_value = "/tmp/";
+
   p.setDefaultString(ROOT_FOLDER, value);
   testAssert("Parameter is default value set", p.isStringSet(ROOT_FOLDER));
   testAssert("Parameter get default value", value.compare(p.getString(ROOT_FOLDER)) == 0);
 
-  std::string new_value = "/tmp/";
-  p.setString(ROOT_FOLDER, new_value);
-  testAssert("Parameter override default value", new_value.compare(p.getString(ROOT_FOLDER)) == 0);
+  p.setString(TEST_DATA_FILE, value);
+  p.setDefaultString(TEST_DATA_FILE, new_value);
+  testAssert("Parameter value not overrided", new_value.compare(p.getString(TEST_DATA_FILE)) != 0);
 }
 
-// void test_parameters_arg_parser();
+void test_parameters_arg_parser() {
+  vector<string> args = {
+      "executable to be ignored", // this line has to be present
+      "--train_data_file", "the/rainbow/",
+      "--heuristic", "1",
+      "--dropout_hyp", "0.4"};
+
+  Parameters p = Parameters(args);
+
+  testAssert("Parameter by user args: parse int", p.getInt(HEURISTIC) == 1);
+  testAssert("Parameter by user args: parse string", p.getString(TRAIN_DATA_FILE).compare("the/rainbow/") == 0);
+  testAssert("Parameter by user args: parse float", p.getFloat(DROPOUT_HYP) == 0.4f);
+
+  args.push_back("--root_folder"); 
+  args.push_back("somehere/over/");
+  testAssert("Parameter by user args: root_folder concat", p.getString(TRAIN_DATA_FILE).compare("somehere/over/the/rainbow/"));
+}
+
 // void test_parameters_json_parser();
 
 void run_fidexcommon_test_suite() {
   test_parameters_setter();
   test_parameters_getter();
   test_parameters_default_setter();
+  test_parameters_arg_parser();
 }
