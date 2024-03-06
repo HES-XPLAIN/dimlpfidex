2f510081e029b132ce087db6d32aa8dcae0596d6
Damian Boquete <damian.boquete@hotmail.com>
2024-03-06 16:39:17 +0100

    Added dimlp and fidex executables tests

--- tests/cpp/headers/dimlpTests.hpp
+++ tests/cpp/headers/dimlpTests.hpp
@@ -2,24 +2,14 @@
 #define DIMLP_TESTS_H
 
 #include "utils.hpp"
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 6adbcd3 (Refactored test folder)
 #include "../../../dimlp/cpp/src/densClsFct.h"
 #include "../../../dimlp/cpp/src/dimlpBTFct.h"
 #include "../../../dimlp/cpp/src/dimlpClsFct.h"
 #include "../../../dimlp/cpp/src/dimlpPredFct.h"
 #include "../../../dimlp/cpp/src/dimlpRulFct.h"
 #include "../../../dimlp/cpp/src/dimlpTrnFct.h"
-<<<<<<< HEAD
-=======
->>>>>>> 5d09d4a (Renamed test folder, added dimlp & fidex test's base functions.)
-=======
 
 const std::string TEMPLATES_PATH = "tests/templates/";
->>>>>>> 6adbcd3 (Refactored test folder)
-
 void testDensCls();
 void testDimlpBT();
 void testDimlpCls();
