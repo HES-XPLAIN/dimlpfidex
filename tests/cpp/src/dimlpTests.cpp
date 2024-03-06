cd8638bbc86af4cbbe430a382ed4f896239f0fe1
Damian Boquete <damian.boquete@hotmail.com>
2024-03-06 15:44:28 +0100

    Refactored test folder

--- tests/cpp/src/dimlpTests.cpp
+++ tests/cpp/src/dimlpTests.cpp
@@ -1,7 +1,10 @@
 #include "../headers/dimlpTests.hpp"
 
+
 void testDensCls() {
   std::cout << "testDensCls() not implemented." << std::endl;
+  std::string command = "--json_config_file " + TEMPLATES_PATH + "config_densCls.json";
+  densCls(command);
 }
 
 void testDimlpBT() {
