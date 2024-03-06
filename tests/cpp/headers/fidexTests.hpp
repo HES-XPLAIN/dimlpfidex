3e29b83cb6e1485d03539884ef8c29e9206b26eb
Damian Boquete <damian.boquete@hotmail.com>
2024-03-06 15:44:28 +0100

    Refactored test folder

--- tests/cpp/headers/fidexTests.hpp
+++ tests/cpp/headers/fidexTests.hpp
@@ -2,14 +2,10 @@
 #define FIDEX_TESTS_H
 
 #include "../../../common/cpp/src/parameters.h"
-<<<<<<< HEAD
 #include "../../../fidex/cpp/src/fidexFct.h"
 #include "../../../fidex/cpp/src/fidexGloFct.h"
 #include "../../../fidex/cpp/src/fidexGloRulesFct.h"
 #include "../../../fidex/cpp/src/fidexGloStatsFct.h"
-=======
-#include "../../../fidex/cpp/src/fidexGloRulesFct.h"
->>>>>>> 5d09d4a (Renamed test folder, added dimlp & fidex test's base functions.)
 #include "utils.hpp"
 
 void testFidex();
