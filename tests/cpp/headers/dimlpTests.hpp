d73a9f8f16cc29ee3def1eda4edeb3816ffc598b
Damian Boquete <damian.boquete@hotmail.com>
2024-03-06 15:44:28 +0100

    Refactored test folder

--- tests/cpp/headers/dimlpTests.hpp
+++ tests/cpp/headers/dimlpTests.hpp
@@ -2,6 +2,14 @@
 #define DIMLP_TESTS_H
 
 #include "utils.hpp"
+#include "../../../dimlp/cpp/src/densClsFct.h"
+#include "../../../dimlp/cpp/src/dimlpBTFct.h"
+#include "../../../dimlp/cpp/src/dimlpClsFct.h"
+#include "../../../dimlp/cpp/src/dimlpPredFct.h"
+#include "../../../dimlp/cpp/src/dimlpRulFct.h"
+#include "../../../dimlp/cpp/src/dimlpTrnFct.h"
+
+const std::string TEMPLATES_PATH = "tests/templates/";
 
 void testDensCls();
 void testDimlpBT();
