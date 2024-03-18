097375b5c09ea383f2383b0428ab50811947d0d9
eldado <damian.boquete-costa@etu.hesge.ch>
2024-03-18 14:30:19 +0100

    Fixed typo

--- tests/cpp/headers/dimlpTests.hpp
+++ tests/cpp/headers/dimlpTests.hpp
@@ -1,15 +1,14 @@
 #ifndef DIMLP_TESTS_H
 #define DIMLP_TESTS_H
 
-#include "utils.hpp"
 #include "../../../dimlp/cpp/src/densClsFct.h"
 #include "../../../dimlp/cpp/src/dimlpBTFct.h"
 #include "../../../dimlp/cpp/src/dimlpClsFct.h"
 #include "../../../dimlp/cpp/src/dimlpPredFct.h"
 #include "../../../dimlp/cpp/src/dimlpRulFct.h"
 #include "../../../dimlp/cpp/src/dimlpTrnFct.h"
+#include "utils.hpp"
 
-const std::string TEMPLATES_PATH = "tests/templates/";
 void testDensCls();
 void testDimlpBT();
 void testDimlpCls();
