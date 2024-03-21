bb51ba7aa3264833359fc7faf2a1a88d271cc311
Damian Boquete <damian.boquete@hotmail.com>
2024-03-21 13:51:01 +0100

    Added tests

--- tests/cpp/src/fidexTests.cpp
+++ tests/cpp/src/fidexTests.cpp
@@ -1,28 +1,49 @@
 #include "../headers/fidexTests.hpp"
 
 uint testFidex() {
-    std::string command = "--json_config_file " + TEMPLATES_PATH + "config_fidex.json";
-  std::string testName = "Simple fidex execution";
+  uint res = 0;
 
-  return testExec(testName, command, fidex);
+  std::string command = "--json_config_file " + TEMPLATES_PATH + "config_fidex_1.json";
+  std::string testName = "Simple fidex execution (with dimlpTrn weights)";
+  res += testExec(testName, command, fidex);
+
+  command = "--json_config_file " + TEMPLATES_PATH + "config_fidex_2.json";
+  testName = "Simple fidex execution (with dimlpBT weights)";
+  res += testExec(testName, command, fidex);
+
+  return res;
 }
 
 uint testFidexGlo() {
-    std::string command = "--json_config_file " + TEMPLATES_PATH + "config_fidexGlo.json";
-  std::string testName = "Simple fidexGlo execution";
+  uint res = 0;
 
-  return testExec(testName, command, fidexGlo);
+  std::string command = "--json_config_file " + TEMPLATES_PATH + "config_fidexGlo_1.json";
+  std::string testName = "Simple fidexGlo execution (with dimlpTrn weights)";
+  res += testExec(testName, command, fidexGlo);
+
+  command = "--json_config_file " + TEMPLATES_PATH + "config_fidexGlo_2.json";
+  testName = "Simple fidexGlo execution (with dimlpBT weights)";
+  res += testExec(testName, command, fidexGlo);
+
+  return res;
 }
 
 uint testFidexGloRules() {
-  std::string command = "--json_config_file " + TEMPLATES_PATH + "config_fidexGloRules.json";
-  std::string testName = "Simple fidexGloRules execution";
+  uint res = 0;
 
-  return testExec(testName, command, fidexGloRules);
+  std::string command = "--json_config_file " + TEMPLATES_PATH + "config_fidexGloRules_1.json";
+  std::string testName = "Simple fidexGloRules execution (with dimlpTrn weights)";
+  res += testExec(testName, command, fidexGloRules);
+
+  command = "--json_config_file " + TEMPLATES_PATH + "config_fidexGloRules_2.json";
+  testName = "Simple fidexGloRules execution (with dimlpBT weights)";
+  res += testExec(testName, command, fidexGloRules);
+
+  return res;
 }
 
 uint testFidexGloStats() {
-    std::string command = "--json_config_file " + TEMPLATES_PATH + "config_fidexGloStats.json";
+  std::string command = "--json_config_file " + TEMPLATES_PATH + "config_fidexGloStats.json";
   std::string testName = "Simple fidexGloStats execution";
 
   return testExec(testName, command, fidexGloStats);
