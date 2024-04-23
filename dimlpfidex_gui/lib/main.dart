765051a5675df09fef61e06d70cb448f59d9b2c4
Damian Boquete <damian.boquete@hotmail.com>
2024-04-23 13:22:41 +0200

    Set minimum screen width on different platforms to avoid UI overflows

--- dimlpfidex_gui/lib/main.dart
+++ dimlpfidex_gui/lib/main.dart
@@ -1,6 +1,16 @@
+import 'dart:io';
+import 'package:window_size/window_size.dart';
+
 import 'views/app_view.dart';
 import 'package:flutter/material.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  if (Platform.isWindows || Platform.isLinux || Platform.isMacOS) {
+    const Size minScreenSize = Size(1100, 800);
+    setWindowMinSize(minScreenSize);
+  }
+
   runApp(const App());
 }
