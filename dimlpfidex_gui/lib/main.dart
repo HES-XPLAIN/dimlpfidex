b3ba3a4adffd2c7a86301fb39dff379d3d7abcac
Damian <49654334+ElSuperDado@users.noreply.github.com>
2024-04-26 11:16:53 +0200

    GUI web fix (#141)
    * Removed test form
    
    * Removed comment
    
    * Modified some input names and descriptions
    
    * Modified common input values
    
    * Updated fields descriptions, names, boundaries, default values. Also added TODOs to improve some fields related features
    
    * Updated all fields, added more TODOs for future features
    
    * Moved validators and transformers in another source file, updated double and integer validators and transformers
    
    * Updated .gitignore
    
    * Set minimum screen width on different platforms to avoid UI overflows
    
    * Changes related to minimal screen width
    
    * Updated GUI .gitignore
    
    * Added content
    
    * Deleted useless file
    
    * Removed empty notebook
    
    * Fixed bug where web version diplays a blank page

--- dimlpfidex_gui/lib/main.dart
+++ dimlpfidex_gui/lib/main.dart
@@ -1,15 +1,17 @@
 import 'dart:io';
-import 'package:window_size/window_size.dart';
-
 import 'views/app_view.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
+import 'package:window_size/window_size.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
 
-  if (Platform.isWindows || Platform.isLinux || Platform.isMacOS) {
-    const Size minScreenSize = Size(1100, 800);
-    setWindowMinSize(minScreenSize);
+  if (!kIsWeb) {
+    if (Platform.isWindows || Platform.isLinux || Platform.isMacOS) {
+      const Size minScreenSize = Size(1100, 800);
+      setWindowMinSize(minScreenSize);
+    }
   }
 
   runApp(const App());
