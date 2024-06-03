b15352d2abbb2adc316a51e15039322e37013227
Jean-Marc-B <jeanmarc.boutay@gmail.com>
2024-06-03 12:00:08 +0200

    Another try for doc Fide

--- fidex/cpp/src/fidexGloStatsFct.cpp
+++ fidex/cpp/src/fidexGloStatsFct.cpp
@@ -168,7 +168,7 @@ void checkStatsParametersLogicValues(Parameters &p) {
  * - The path of the global rules output file must be provided to compute statistics of the rules on the test set.
  * - If the positive class index is specified, the true/false positive/negative rates are computed.
  * - Parameters can be defined directly via the command line or through a JSON configuration file.
- * - Providing no command-line arguments or using -h/\--\--help displays usage instructions, detailing both required and optional parameters for user guidance.
+ * - Providing no command-line arguments or using -h/&ndash;&ndash;help displays usage instructions, detailing both required and optional parameters for user guidance.
  *
  * File formats:
  * - Data files should contain one sample per line, with numbers separated either by spaces, tabs, semicolons, or commas. Supported formats:
@@ -185,7 +185,7 @@ void checkStatsParametersLogicValues(Parameters &p) {
  *   The second line indicates if a decision threshold has been used. If no, it says: 'No decision threshold is used.'
  *   and if yes, it says something like 'Using a decision threshold of 0.3 for class 0'.
  *   Then there is an empty line and each rule is numbered starting from 1 and separated from each other by an empty line. A rule is in the form:<br>
- *   Rule 1: X2531>=175.95 X2200>=181.05 X1828>=175.95 X2590>=178.5 X1257>=183.6 X2277>=170.85 X1816>=173.4 X3040>=183.6 -> class 0<br>
+ *   "Rule" 1: X2531>=175.95 X2200>=181.05 X1828>=175.95 X2590>=178.5 X1257>=183.6 X2277>=170.85 X1816>=173.4 X3040>=183.6 -> class 0<br>
  *   Train Covering size : 127<br>
  *   Train Fidelity : 1<br>
  *   Train Accuracy : 1<br>
@@ -195,7 +195,7 @@ void checkStatsParametersLogicValues(Parameters &p) {
  *   The order is important as the first attribute/class name will represent the first attribute/class in the dataset.
  *
  * Example of how to call the function:<br>
- * fidex.fidexGloStats('\--\--test_data_file datanormTest.txt \--\--test_pred_file predTest.out \--\--test_class_file dataclass2Test.txt \--\--global_rules_file globalRules.rls \--\--nb_attributes 16 \--\--nb_classes 2 \--\--stats_file stats.txt \--\--root_folder dimlp/datafiles')
+ * fidex.fidexGloStats('&ndash;&ndash;test_data_file datanormTest.txt &ndash;&ndash;test_pred_file predTest.out &ndash;&ndash;test_class_file dataclass2Test.txt &ndash;&ndash;global_rules_file globalRules.rls &ndash;&ndash;nb_attributes 16 &ndash;&ndash;nb_classes 2 &ndash;&ndash;stats_file stats.txt &ndash;&ndash;root_folder dimlp/datafiles')
  *
  * @param command A single string containing either the path to a JSON configuration file with all specified arguments, or all arguments for the function formatted like command-line input. This includes file paths and options for output.
  * @return Returns 0 for successful execution, -1 for errors encountered during the process.
