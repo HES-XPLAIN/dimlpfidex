038ced750ce0f00d146e35935cde33d2154a1188
Jean-Marc-B <jeanmarc.boutay@gmail.com>
2024-06-03 11:36:27 +0200

    Nre try for Fidex doc

--- fidex/cpp/src/fidexFct.cpp
+++ fidex/cpp/src/fidexFct.cpp
@@ -108,16 +108,11 @@ void checkFidexParametersLogicValues(Parameters &p) {
  * classes of samples and constructing a rule for each test sample based on these hyperplanes covering this sample
  * and as many other samples as possible.
  *
- * The `Fidex` algorithm is computed until a rule is created or until the max failed attempts limit is reached.
- * \par
- * -> First attempt to generate a rule with a covering greater or equal to 'min_covering' and a fidelity greater or equal to 'min_fidelity'.
- * \par
- * -> If the attempt failed and the 'covering_strategy' is on, Fidex is computed to find a rule with the max possible minimal covering that can be lower than 'min_covering'.
- * \par
- * -> If all attempts failed, the targeted fidelity is gradually lowered until it succeed or 'lowest_min_fidelity' is reached.
- * \par
- * -> Each failed attempt on lowest minimal fidelity are counted.
- * \par
+ * The \c Fidex algorithm is computed until a rule is created or until the max failed attempts limit is reached.<br>
+ * -> First attempt to generate a rule with a covering greater or equal to 'min_covering' and a fidelity greater or equal to 'min_fidelity'.<br>
+ * -> If the attempt failed and the 'covering_strategy' is on, Fidex is computed to find a rule with the max possible minimal covering that can be lower than 'min_covering'.<br>
+ * -> If all attempts failed, the targeted fidelity is gradually lowered until it succeed or 'lowest_min_fidelity' is reached.<br>
+ * -> Each failed attempt on lowest minimal fidelity are counted.<br>
  * -> If the max failed attempts limit is reached, then the rule couldn't be computed for this sample.
  *
  * Notes:
@@ -129,7 +124,7 @@ void checkFidexParametersLogicValues(Parameters &p) {
  * - The path of the file containing the computed rules must be provided. It can be generated as a JSON if a JSON extension is specified.
  * - Normalization parameters can be specified to denormalize the rules if data were normalized beforehand.
  * - Parameters can be defined directly via the command line or through a JSON configuration file.
- * - Providing no command-line arguments or using -h/\--help displays usage instructions, detailing both required and optional parameters for user guidance.
+ * - Providing no command-line arguments or using -h/\--\--help displays usage instructions, detailing both required and optional parameters for user guidance.
  *
  * File formats:
  * - Data files should contain one sample per line, with numbers separated either by spaces, tabs, semicolons, or commas. Supported formats:
@@ -152,15 +147,12 @@ void checkFidexParametersLogicValues(Parameters &p) {
  * - Attributes file: Each line corresponds to one attribute, each attribute must be specified. Classes can be specified
  *   after the attributes but are not mandatory. Each attribute or class must be in one word without spaces (you can use _ to replace a space).
  *   The order is important as the first attribute/class name will represent the first attribute/class in the dataset.
- * - Normalization file: Each line contains the mean/median and standard deviation for an attribute.
- * \par
- *   Format: '2 : original mean: 0.8307, original std: 0.0425'
- * \par
+ * - Normalization file: Each line contains the mean/median and standard deviation for an attribute.<br>
+ *   Format: '2 : original mean: 0.8307, original std: 0.0425'<br>
  *   Attribute indices (index 2 here) can be replaced with attribute names, then an attribute file is required.
  *
- * Example of how to call the function:
- * \par
- * `fidex.fidex('\--train_data_file datanorm \--train_pred_file dimlp.out \--train_class_file dataclass2 \--test_data_file testSampleDataCombine \--nb_attributes 16 \--nb_classes 2 \--weights_file dimlp.wts \--nb_quant_levels 50 \--rules_outfile rule.txt \--stats_file stats \--max_iterations 100 \--min_covering 25 \--dropout_dim 0.5 \--dropout_hyp 0.5 \--root_folder fidex/datafiles')`
+ * Example of how to call the function:<br>
+ * `fidex.fidex('\--\--train_data_file datanorm \--\--train_pred_file dimlp.out \--\--train_class_file dataclass2 \--\--test_data_file testSampleDataCombine \--\--nb_attributes 16 \--\--nb_classes 2 \--\--weights_file dimlp.wts \--\--nb_quant_levels 50 \--\--rules_outfile rule.txt \--\--stats_file stats \--\--max_iterations 100 \--\--min_covering 25 \--\--dropout_dim 0.5 \--\--dropout_hyp 0.5 \--\--root_folder fidex/datafiles')`
  *
  * @param command A single string containing either the path to a JSON configuration file with all specified arguments, or all arguments for the function formatted like command-line input. This includes file paths, Fidex parameters, and options for output.
  * @return Returns 0 for successful execution, -1 for errors encountered during the process.
