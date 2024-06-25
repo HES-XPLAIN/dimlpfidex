96924b49c69ee0d9b4e935afd1e9c734d375a087
Damian Boquete <damian.boquete@hotmail.com>
2024-06-25 11:32:43 +0200

    Imports are now more straight forward

---
+++ trainings/__init__.py
@@ -0,0 +1,9 @@
+from .cnnTrn import cnnTrn
+from .svmTrn import svmTrn
+from .mlpTrn import mlpTrn
+from .gradBoostTrn import gradBoostTrn
+from .randForestsTrn import randForestsTrn
+from .normalization import normalization
+from .computeRocCurve import computeRocCurve
+
+__all__ = ['cnnTrn', 'svmTrn', 'mlpTrn', 'gradBoostTrn', 'randForestsTrn', 'normalization', 'computeRocCurve']
