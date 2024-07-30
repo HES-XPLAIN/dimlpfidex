dae7ea415d2c7419fcc922f3e4894c50dd4d5366
ElSuperDado <damian.boquete@hotmail.com>
2024-07-30 15:37:18 +0200

    Starting to update field system to adopt multi datatype inputs, wip

--- dimlpfidex_gui/lib/data/field.dart
+++ dimlpfidex_gui/lib/data/field.dart
@@ -95,3 +95,115 @@ class Field {
     ]);
   }
 }
+
+
+
+class UnstableField {
+  final String label;
+  final String jsonLabel;
+  final bool isRequired;
+  final List<Metadata> metadatas;
+
+  const UnstableField(this.label, this.jsonLabel, this.isRequired,this.metadatas);
+
+  Widget toInputField() {
+    return InputField(field: this);
+  }
+
+  GlossaryCard toGlossaryCard() {
+    return GlossaryCard(field: this);
+  }
+
+  // used by the glossary card builder
+  DataTable toDataTable() {
+    return DataTable(columns: const [
+      DataColumn(
+          label: Expanded(
+        child: Text(
+          "Property",
+          style: TextStyle(fontStyle: FontStyle.italic),
+        ),
+      )),
+      DataColumn(
+          label: Expanded(
+        child: Text(
+          "Value",
+          style: TextStyle(fontStyle: FontStyle.italic),
+        ),
+      ))
+    ], rows: [
+      DataRow(cells: [
+        const DataCell(Text("JSON label")),
+        DataCell(Text(jsonLabel))
+      ]),
+      DataRow(cells: [
+        const DataCell(Text("Is required")),
+        DataCell(Text(isRequired.toString()))
+      ]),
+      if (datatype == Datatype.restrictedChoiceString)
+        DataRow(cells: [
+          const DataCell(Text("Choices")),
+          DataCell(Text(items.isEmpty
+              ? "None"
+              : items.toString().replaceAll(RegExp(r'(\[|\])+'), '')))
+        ]),
+      DataRow(cells: [
+        const DataCell(Text("Default value")),
+        DataCell(Text(defaultValue != "" ? defaultValue : "None"))
+      ]),
+      if (datatype == Datatype.integer || datatype == Datatype.doublePrecision)
+        DataRow(cells: [
+          const DataCell(Text("Min value")),
+          DataCell(Text(minValue != "" ? minValue : "None"))
+        ]),
+      if (datatype == Datatype.integer || datatype == Datatype.doublePrecision)
+        DataRow(cells: [
+          const DataCell(Text("Max value")),
+          DataCell(Text(maxValue != "" ? maxValue : "None"))
+        ])
+    ]);
+  }
+}
+
+
+class Metadata {
+  final Datatype datatype;
+  final String description;
+  final String defaultValue;
+  final String minValue;
+  final String maxValue;
+  final List<String> items;
+
+  const Metadata(this.datatype,
+      {this.defaultValue = "",
+      this.minValue = "",
+      this.maxValue = "",
+      this.items = const [],
+      this.description = "No description available"});
+
+  List<DataRow> toDataTable() {
+    return [
+      if (datatype == Datatype.restrictedChoiceString)
+        DataRow(cells: [
+          const DataCell(Text("Choices")),
+          DataCell(Text(items.isEmpty
+              ? "None"
+              : items.toString().replaceAll(RegExp(r'(\[|\])+'), '')))
+        ]),
+      DataRow(cells: [
+        const DataCell(Text("Default value")),
+        DataCell(Text(defaultValue != "" ? defaultValue : "None"))
+      ]),
+      if (datatype == Datatype.integer || datatype == Datatype.doublePrecision)
+        DataRow(cells: [
+          const DataCell(Text("Min value")),
+          DataCell(Text(minValue != "" ? minValue : "None"))
+        ]),
+      if (datatype == Datatype.integer || datatype == Datatype.doublePrecision)
+        DataRow(cells: [
+          const DataCell(Text("Max value")),
+          DataCell(Text(maxValue != "" ? maxValue : "None"))
+        ])
+    ];
+  }
+}
