3da839c126da35c7392d239d5b9533834edda53b
Damian Boquete <damian.boquete@hotmail.com>
2024-07-31 15:29:15 +0200

    Added buggy carousel for fields

--- dimlpfidex_gui/lib/data/field.dart
+++ dimlpfidex_gui/lib/data/field.dart
@@ -1,5 +1,6 @@
 import 'package:dimlpfidex_gui/ui/glossary_card.dart';
 import 'package:dimlpfidex_gui/ui/input_field.dart';
+import 'package:dimlpfidex_gui/ui/input_unstable_field.dart';
 import 'package:flutter/material.dart';
 
 enum Datatype {
@@ -107,61 +108,7 @@ class UnstableField {
   const UnstableField(this.label, this.jsonLabel, this.isRequired,this.metadatas);
 
   Widget toInputField() {
-    return InputField(field: this);
-  }
-
-  GlossaryCard toGlossaryCard() {
-    return GlossaryCard(field: this);
-  }
-
-  // used by the glossary card builder
-  DataTable toDataTable() {
-    return DataTable(columns: const [
-      DataColumn(
-          label: Expanded(
-        child: Text(
-          "Property",
-          style: TextStyle(fontStyle: FontStyle.italic),
-        ),
-      )),
-      DataColumn(
-          label: Expanded(
-        child: Text(
-          "Value",
-          style: TextStyle(fontStyle: FontStyle.italic),
-        ),
-      ))
-    ], rows: [
-      DataRow(cells: [
-        const DataCell(Text("JSON label")),
-        DataCell(Text(jsonLabel))
-      ]),
-      DataRow(cells: [
-        const DataCell(Text("Is required")),
-        DataCell(Text(isRequired.toString()))
-      ]),
-      if (datatype == Datatype.restrictedChoiceString)
-        DataRow(cells: [
-          const DataCell(Text("Choices")),
-          DataCell(Text(items.isEmpty
-              ? "None"
-              : items.toString().replaceAll(RegExp(r'(\[|\])+'), '')))
-        ]),
-      DataRow(cells: [
-        const DataCell(Text("Default value")),
-        DataCell(Text(defaultValue != "" ? defaultValue : "None"))
-      ]),
-      if (datatype == Datatype.integer || datatype == Datatype.doublePrecision)
-        DataRow(cells: [
-          const DataCell(Text("Min value")),
-          DataCell(Text(minValue != "" ? minValue : "None"))
-        ]),
-      if (datatype == Datatype.integer || datatype == Datatype.doublePrecision)
-        DataRow(cells: [
-          const DataCell(Text("Max value")),
-          DataCell(Text(maxValue != "" ? maxValue : "None"))
-        ])
-    ]);
+    return InputUnstableField(field: this);
   }
 }
 
